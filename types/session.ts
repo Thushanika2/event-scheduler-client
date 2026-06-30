6feee265137f7bbd8e7e89ab8e635a39f39bbfe3
Shanujan <shanushanujan234@gmail.com>
2026-06-30 15:32:49 +0530

    session.ts created

---
+++ types/session.ts
@@ -0,0 +1,10 @@
+export type Session = {
+  session_id: number
+  room_id: number
+  title: string
+  speaker: string
+  capacity: number
+  date: string
+  start_time: string
+  end_time: string
+}
